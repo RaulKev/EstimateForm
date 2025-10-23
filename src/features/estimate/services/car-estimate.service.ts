f86735440a8dda5503970f7fe29da5eeedd72449
Angelo Salazar <angelo@prodequa.com>
2025-10-23 17:39:21 -0500

    Implementar generación de cotización de seguros y agregar interfaz de respuesta de seguro

--- src/features/estimate/services/car-estimate.service.ts
+++ src/features/estimate/services/car-estimate.service.ts
@@ -1,6 +1,22 @@
 import { carsList } from "@/mocks/car-data.mock";
 import type { CarListResponse } from "../type/types";
+import { insuranceResponse, type Insurance } from "@/mocks/request.mock";
+import type { EstimateFormData } from "../config/EstimeFormConfig";
 
 export const getCars = async (): Promise<CarListResponse[]> => {
   return carsList;
+};
+
+export const generateInsurance = async (data: EstimateFormData): Promise<Insurance> => {
+  
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const randomNumber = Math.floor(Math.random() * 2) + 1;
+      if (randomNumber === 1) {
+        resolve(insuranceResponse);
+      } else {
+        throw new Error("Error al cotizar");
+      }
+    }, 400);
+  });
 };
